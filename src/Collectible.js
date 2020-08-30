75eadec0e70989c23e40bee3a75e2b1c613e270e
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-29 20:15:49 -0400

    create random food & add isCarried bool

--- src/Collectible.js
+++ src/Collectible.js
@@ -3,37 +3,37 @@ import GameObject from './GameObject.js';
 export default class Collectible extends GameObject {
   static all = [];
 
-  static createRandomFood(srcX, srcY) {
+  static createRandomFood(x, y) {
     // TODO set random x, y that's not somewhere another object currently is located
-    const donut = Collectible.createDonut(150, 150);
-    const pizza = Collectible.createPizza(300, 300);
-    const cookie = Collectible.createCookie(600, 350);
-    const taco = Collectible.createTaco(100, 400);
+    const randIndex = Math.floor(Math.random() * Collectible.types.length);
+    Collectible.types[randIndex](x, y);
   }
 
-  constructor(srcX, srcY, srcW, srcH, x, y, w, h, type, currentAnim = null) {
+  constructor(srcX, srcY, srcW, srcH, x, y, w, h, type = 'collectible', currentAnim = null) {
     super(srcX, srcY, srcW, srcH, x, y, w, h, type, currentAnim);
-
+    this.isCarried = false;
 
     Collectible.all.push(this);
     console.log(Collectible.all)
   }
 
+  static types = [
+    Collectible.createDonut, Collectible.createPizza, Collectible.createCookie, Collectible.createTaco
+  ]
+
   static createDonut(x, y) {
-    return new Collectible(131, 19, 10, 10, x, y, 10, 10, 'donut');
+    return new Collectible(131, 19, 10, 10, x, y, 10, 10);
   }
 
-  static createPizza(x, y) {
-    return new Collectible(147, 19, 10, 10, x, y, 10, 10, 'pizza');
-  }
+  // static createPizza(x, y) {
+  //   return new Collectible(147, 19, 10, 10, x, y, 10, 10);
+  // }
 
-  static createCookie(x, y) {
-    return new Collectible(163, 19, 10, 10, x, y, 10, 10, 'cookie');
-  }
+  // static createCookie(x, y) {
+  //   return new Collectible(163, 19, 10, 10, x, y, 10, 10);
+  // }
 
   static createTaco(x, y) {
-    return new Collectible(179, 19, 10, 7, x, y, 10, 7, 'taco');
+    return new Collectible(179, 19, 10, 7, x, y, 10, 7);
   }
-
-
 }
