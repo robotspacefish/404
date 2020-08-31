42c477ee7b06a1487fe12836161212e7fbcbb749
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-30 21:00:57 -0400

    move smoothing setting from this file

--- src/canvas.js
+++ src/canvas.js
@@ -2,10 +2,7 @@ const canvas = document.getElementById('canvas'),
   bgCanvas = document.getElementById('bg-canvas'),
   ctx = canvas.getContext('2d'),
   bgCtx = bgCanvas.getContext('2d');
-// HEIGHT = canvas.height,
-// WIDTH = canvas.width;
-ctx.imageSmoothingEnabled = false; // remove blurring from resizing
-bgCtx.imageSmoothingEnabled = false; // remove blurring from resizing
+
 const HEIGHT = () => canvas.height;
 const WIDTH = () => canvas.width;
 export { canvas, ctx, bgCanvas, bgCtx, HEIGHT, WIDTH };
