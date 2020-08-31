34883b8e854e2eef5fb489f650d599d314176a39
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-30 21:37:47 -0400

    spawn foods in their food courts

--- src/Food.js
+++ src/Food.js
@@ -1,31 +1,41 @@
 import GameObject from './GameObject.js';
+import gameMap from './map.js';
+import { config } from './config.js';
 
 export default class Food extends GameObject {
   static all = [];
-
-  static createRandomFood(x, y) {
-    // TODO set random x, y that's not somewhere another object currently is located
-    const randIndex = Math.floor(Math.random() * Food.types.length);
-    Food.types[randIndex](x, y);
-  }
 
   constructor(srcX, srcY, srcW, srcH, x, y, w, h, type = 'Food', currentAnim = null) {
     super(srcX, srcY, srcW, srcH, x, y, w, h, type, currentAnim);
     this.isCarried = false;
 
     Food.all.push(this);
-    // console.log(Food.all)
+  }
+
+  static spawn() {
+    // TODO handle conditions for spawning
+    // const randIndex = Math.floor(Math.random() * Food.types.length);
+    // Food.types[randIndex](x, y);
+    Food.createTaco();
+    Food.createDonut();
   }
 
   static types = [
     Food.createDonut, Food.createTaco
   ]
 
-  static createDonut(x, y) {
-    return new Food(259, 3, 10, 10, x, y, 10, 10);
+  static createDonut() {
+    // return new Food(259, 3, 10, 10, 115 * config.scale, 134 * config.scale, 10 * config.scale, 10 * config.scale);
+    const scale = config.scale;
+    return new Food(259, 6, 10, 10, 115, 134, 10, 10);
+
+
+
   }
 
-  static createTaco(x, y) {
-    return new Food(275, 4, 10, 7, x, y, 10, 7);
+  static createTaco() {
+    return new Food(275, 8, 10, 7, 99, 136, 10, 7);
   }
+
+
 }
