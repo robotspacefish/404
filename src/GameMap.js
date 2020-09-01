c2764dcdf70ee2e4189ee39fe33f6de9147d8fe0
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-09-01 14:32:10 -0400

    comment out isDrawn condition

--- src/GameMap.js
+++ src/GameMap.js
@@ -48,40 +48,40 @@ export default class GameMap {
   }
 
   draw() {
-    if (!this.isDrawn) {
-      // console.log('drawing')
-      const { FLOOR, WALL, TACO_COURT, DONUT_COURT, ENEMY_SPAWN } = mapCodes;
-      let ts = this.tileSize, cell;
-      for (let row = 0; row < gameMap[0].length; row++) {
-        for (let col = 0; col < gameMap.length; col++) {
-          switch (gameMap[col][row]) {
-            case FLOOR:
-              cell = this.floor;
-              break;
-            case WALL:
-              cell = this.wall;
-              break;
-            case TACO_COURT:
-              cell = this.tacoCourt;
-              break
-            case DONUT_COURT:
-              cell = this.donutCourt;
-              break;
-            case ENEMY_SPAWN:
-              cell = this.enemySpawn;
-              break;
-          }
-          bgCtx.drawImage(
-            tilesheet, cell.srcX, cell.srcY, ts, ts,
-            row * ts * config.scale,
-            col * ts * config.scale,
-            ts * config.scale,
-            ts * config.scale
-          );
+    // if (!this.isDrawn) {
+    // console.log('drawing')
+    const { FLOOR, WALL, TACO_COURT, DONUT_COURT, ENEMY_SPAWN } = mapCodes;
+    let ts = this.tileSize, cell;
+    for (let row = 0; row < gameMap[0].length; row++) {
+      for (let col = 0; col < gameMap.length; col++) {
+        switch (gameMap[col][row]) {
+          case FLOOR:
+            cell = this.floor;
+            break;
+          case WALL:
+            cell = this.wall;
+            break;
+          case TACO_COURT:
+            cell = this.tacoCourt;
+            break
+          case DONUT_COURT:
+            cell = this.donutCourt;
+            break;
+          case ENEMY_SPAWN:
+            cell = this.enemySpawn;
+            break;
         }
+        bgCtx.drawImage(
+          tilesheet, cell.srcX, cell.srcY, ts, ts,
+          row * ts * config.scale,
+          col * ts * config.scale,
+          ts * config.scale,
+          ts * config.scale
+        );
       }
-
-      this.isDrawn = true;
     }
+
+    // this.isDrawn = true;
+    // }
   }
 }
