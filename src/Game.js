0d98dbadba23e9c25ec0f65f5d9cc086636e8015
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-09-01 23:00:37 -0400

    WIP resizing

--- src/Game.js
+++ src/Game.js
@@ -18,21 +18,60 @@ export default class Game {
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
     this.scale = 1;
+
+    // logical
+    this.width = 224;
+    this.height = 288;
+    this.maxWidth = 224 * 3;
+    this.maxHeight = 228 * 3;
   }
 
   resize() {
+    // debugger
+    let windowWidth = window.innerHeight,
+      windowHeight = window.innerWidth,
+      canvasWidth, canvasHeight;
+
+    // how many times the native resolution can be scaled up evenly, or max 3 times
+    const scale = Math.min(Math.floor(windowWidth / this.width), 3),
+      scaledWidth = this.width * (scale || 1),
+      scaledHeight = this.height * (scale || 1);
+
+    // const windowRatio = windowWidth / windowHeight;
+    // const gameWindowRatio = this.height / this.width;
+
+    // if (gameWindowRatio < windowRatio) {
+    //   const width = Math.floor(windowHeight * gameWindowRatio);
+    //   canvasWidth = `${width > scaledWidth ? scaledWidth : width}px`;
+    //   canvasHeight = `${width > scaledWidth ? scaledHeight : windowHeight}px`;
+    // } else {
+    //   const height = Math.floor(windowWidth / gameWindowRatio);
+    //   canvasWidth = `${height > scaledHeight ? scaledHeight : windowWidth}px`;
+    //   canvasHeight = `${height > scaledHeight ? scaledHeight : height}px`;
+    // }
+
+    this.canvas.style.width = `${scaledWidth}px`;
+    this.canvas.style.height = `${scaledHeight}px`;
+    gameDiv.style.width = `${scaledWidth}px`;
+    gameDiv.style.height = `${scaledHeight}px`;
+
+    // const ratio = Math.floor(this.height / this.width);
+
+
+    // canvasHeight < canvasWidth / ratio ?
+    //   canvasWidth = Math.floor(canvasHeight * ratio) :
+    //   canvasHeight = Math.floor(canvasWidth / ratio)
+
+    // debugger
+    // this.canvas.style.width = `${canvasWidth}px`;
+    // this.canvas.style.height = `${canvasHeight}px`;
+    // gameDiv.style.width = `${canvasWidth}px`;
+    // gameDiv.style.height = `${canvasHeight}px`;
+
     this.ctx.imageSmoothingEnabled = false; // remove blurring from resizing
-    const canvasWidth = this.canvasWidth, canvasHeight = this.canvasHeight;
-    const windowWidth = window.innerWidth;
-    // const windowHeight = window.innerHeight;
 
-    this.scale = Math.min(Math.floor(windowWidth / canvasWidth), 3);
+    console.log(`render: ${this.canvas.style.width} x ${this.canvas.style.height}`)
 
-    this.canvas.width = canvasWidth * (this.scale || 1);
-    this.canvas.height = canvasHeight * (this.scale || 1);
-
-    gameDiv.style.width = `${canvas.width}px`;
-    gameDiv.style.height = `${canvas.height}px`;
   }
 
   get canvasHeight() {
