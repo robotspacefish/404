71460a2ad48139e70b6de0671a6e04696698452c
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-09-02 16:25:35 -0400

    Enemy class

--- src/Enemy.js
+++ src/Enemy.js
@@ -1,23 +1,86 @@
-export default class Enemy {
-  constructor(x, y, height, width) {
-    this.x = x;
-    this.y = y;
-    this.height = height;
-    this.width = width;
+import GameObject from './GameObject.js';
+import { mapCodes } from './map.js';
+
+const { ENEMY_TYPE_1, ENEMY_TYPE_2, WALL } = mapCodes;
+
+export default class Enemy extends GameObject {
+  static all = [];
+
+  constructor(srcX, srcY, srcW, srcH, x, y, w, h, type, currentAnim) {
+    super(srcX, srcY, srcW, srcH, x, y, w, h, type, currentAnim);
+    this.vx = 0;
+    this.vy = 0;
+
+    this.speed = 2;
     this.want;
 
-    const colors = ['red', 'green', 'purple', 'yellow'];
-    this.color = colors[Math.floor(Math.random() * colors.length)];
+    Enemy.all.push(this);
   }
 
-  update(pX, pY) {
-    // TODO move in the direction of the player
-    // for now, do this
-    this.y += 1;
+  static type1() {
+    const anims = {
+      DOWN: {
+        srcX: 0,
+        srcY: 16,
+      },
+      UP: {
+        srcX: 32,
+        srcY: 16,
+      },
+      RIGHT_SIDE: {
+        srcX: 64,
+        srcY: 16,
+      },
+      LEFT_SIDE: {
+        srcX: 96,
+        srcY: 16,
+      }
+    };
+    return {
+      srcX: 0,
+      srcY: 16,
+      want: 'taco',
+      anims
+    }
   }
 
-  draw() {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.width, this.height)
+  static type2() {
+    const anims = {
+      DOWN: {
+        srcX: 128,
+        srcY: null,
+      },
+      UP: {
+        srcX: 160,
+        srcY: null,
+      },
+      RIGHT_SIDE: {
+        srcX: 192,
+        srcY: null,
+      },
+      LEFT_SIDE: {
+        srcX: 224,
+        srcY: null,
+      }
+    };
+
+    return {
+      srcX: 128,
+      srcY: 16,
+      want: 'donut',
+      anims
+    }
   }
+
+  static spawn(x = 0, y = 0) {
+    const t = Math.random() > 0.5 ? Enemy.type1() : Enemy.type2();
+
+    const enemy = new Enemy(t.srcX, t.srcY, 16, 16, x, y, 16, 16, 'enemy', t.anims.DOWN);
+    enemy.want = t.want;
+  }
+
+  update() {
+
+  }
+
 }
