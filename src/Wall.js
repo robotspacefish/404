8b8ff8c5b46712eebfeddcfe7fdddc2e4125ea7f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-09-03 00:01:13 -0400

    remove anim from wall

--- src/Wall.js
+++ src/Wall.js
@@ -3,8 +3,8 @@ import GameObject from './GameObject.js';
 export default class Wall extends GameObject {
   static all = [];
 
-  constructor(srcX, srcY, srcW, srcH, x, y, w, h, type = 'wall', currentAnim = null) {
-    super(srcX, srcY, srcW, srcH, x, y, w, h, type, currentAnim);
+  constructor(srcX, srcY, srcW, srcH, x, y, w, h, type = 'wall') {
+    super(srcX, srcY, srcW, srcH, x, y, w, h, type);
 
     Wall.all.push(this);
   }
