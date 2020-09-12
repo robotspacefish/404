b0a34f28fadbf74d832436fb037cec05a80b6aa7
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-09-12 00:34:13 -0400

    add controls to instructions

--- src/main.js
+++ src/main.js
@@ -88,6 +88,8 @@ function instructions() {
     You are a defense robot tasked with stopping an alien invasion, but your kill.exe file is missing. In order to get the aliens to leave you have to give them what they want: food!
 
     Make sure to bring the right food to each alien or the food will be you!
+
+    Controls: WASD | ZQSD | Arrow Keys
   `;
 }
 
